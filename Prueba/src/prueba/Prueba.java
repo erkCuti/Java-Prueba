306d1868be444a639fbceec2b574f1c3828a9125
Juan Pablo Carrion <jpcarrion19@gmail.com>
2022-07-22 11:47:59 -0500

    elimina comentarios

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package prueba;
 
-/**
- *
- * @author MATUTINO
- */
 public class Prueba {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
  
         
